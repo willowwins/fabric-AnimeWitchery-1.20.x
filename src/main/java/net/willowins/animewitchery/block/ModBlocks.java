e9988fed49c6a20ab8462b73d5ec1ea723a69067
akzisme <allenkzitting@gmail.com>
2025-05-14 09:18:02 -0600

    Merge remote-tracking branch 'origin/master'

--- src/main/java/net/willowins/animewitchery/block/ModBlocks.java
+++ src/main/java/net/willowins/animewitchery/block/ModBlocks.java
@@ -24,7 +24,7 @@ public class ModBlocks {
             new EffigyFountainBlock(FabricBlockSettings.copyOf(Blocks.STONE).nonOpaque()));
 
 public static final Block BINDING_SPELL =registerBlock("binding_spell",
-            new BindingSpellBlock(FabricBlockSettings.copyOf(Blocks.STONE).nonOpaque()));
+            new BindingSpellBlock(FabricBlockSettings.copyOf(Blocks.STONE).nonOpaque().hardness(-1f)));
 
     public static final Block ACTIVE_EFFIGY_FOUNTAIN =registerBlock("active_effigy_fountain",
             new ActiveEffigyFountainBlock(FabricBlockSettings.copyOf(Blocks.STONE).nonOpaque().hardness(-1f)));
