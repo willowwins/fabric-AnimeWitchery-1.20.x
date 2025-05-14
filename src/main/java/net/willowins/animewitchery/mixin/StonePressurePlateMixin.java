275413fbb700deb94eb1c3919f20b7d901791c32
akzisme <allenkzitting@gmail.com>
2025-05-14 09:18:02 -0600

    yummy recipes

---
+++ src/main/java/net/willowins/animewitchery/mixin/StonePressurePlateMixin.java
@@ -0,0 +1,36 @@
+package net.willowins.animewitchery.mixin;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.willowins.animewitchery.block.ModBlocks;
+import net.willowins.animewitchery.item.ModItems;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractBlock.class)
+public class StonePressurePlateMixin {
+
+    @Inject(method = "onUse", at = @At("HEAD"))
+    private void onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+        if (world.getBlockState(pos).isOf(Blocks.STONE_PRESSURE_PLATE) && !world.isClient) {
+            if (player.getMainHandStack().isOf(ModItems.ALCHEMICAL_CATALYST)){
+                world.setBlockState(pos,ModBlocks.BINDING_SPELL.getDefaultState());
+                player.getMainHandStack().decrement(1);
+                player.getOffHandStack().decrement(1);
+
+
+            }
+        }
+    }
+
+}
