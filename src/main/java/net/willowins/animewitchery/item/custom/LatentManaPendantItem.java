3847bc981bdb0ab6957c4484ea85dda55372e5df
akzisme <allenkzitting@gmail.com>
2025-10-21 17:37:18 -0600

    Increase Latent Mana Pendant to +20k mana and add second necklace slot

--- src/main/java/net/willowins/animewitchery/item/custom/LatentManaPendantItem.java
+++ src/main/java/net/willowins/animewitchery/item/custom/LatentManaPendantItem.java
@@ -11,7 +11,7 @@ import net.minecraft.world.World;
 import java.util.List;
 
 public class LatentManaPendantItem extends TrinketItem {
-    private static final int BONUS_MANA = 5000;
+    private static final int BONUS_MANA = 20000;
     
     public LatentManaPendantItem(Settings settings) {
         super(settings);
@@ -20,7 +20,7 @@ public class LatentManaPendantItem extends TrinketItem {
     @Override
     public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
         tooltip.add(Text.literal("Expands latent mana pool").formatted(Formatting.LIGHT_PURPLE));
-        tooltip.add(Text.literal("+5000 Maximum Mana").formatted(Formatting.AQUA));
+        tooltip.add(Text.literal("+20,000 Maximum Mana").formatted(Formatting.AQUA));
         tooltip.add(Text.literal("Must be worn in necklace slot").formatted(Formatting.GRAY));
     }
     
